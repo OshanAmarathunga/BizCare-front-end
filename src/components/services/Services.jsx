1354a6d67abbeb06cf26bd2c64fc9216c22531cd
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2024-11-12 23:58:50 +0530

    font size changed

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -14,51 +14,51 @@ export default function Services() {
           <ul className="list-disc text-white pl-5 md:flex sm:flex-row">
             <div className="flex-col justify-between">
               <li className="mb-4 mx-4">
-                <div className="font-semibold text-white hover:text-yellow-500">
+                <div className="text-[10px] md:text-[15px] font-light text-white hover:text-yellow-500">
                   New Company Registrations
                 </div>
               </li>
               <li className="mb-4 mx-4">
-                <div className="font-semibold text-white hover:text-yellow-500">
+                <div className="text-[10px] md:text-[15px] font-light text-white hover:text-yellow-500">
                   Secretarial Works
                 </div>
               </li>
               <li className="mb-4 mx-4">
-                <div className=" text-white font-semibold hover:text-yellow-500">
+                <div className=" text-white text-[10px] md:text-[15px] font-light hover:text-yellow-500">
                   Income Tax Consultation
                 </div>
               </li>
             </div>
             <div className="flex-col  justify-between">
               <li className="mb-4 mx-4">
-                <div className="font-semibold text-white hover:text-yellow-500">
+                <div className="text-[10px] md:text-[15px] font-light text-white hover:text-yellow-500">
                   Company Audit{" "}
                 </div>
               </li>
               <li className="mb-4 mx-4">
-                <div className="font-semibold text-white hover:text-yellow-500">
+                <div className="text-[10px] md:text-[15px] font-light text-white hover:text-yellow-500">
                   Book Keeping
                 </div>
               </li>
               <li className="mb-4 mx-4">
-                <div className="font-semibold text-white hover:text-yellow-500">
+                <div className="text-[10px] md:text-[15px] font-light text-white hover:text-yellow-500">
                   Preparation of Management Accounts & Final Accounts
                 </div>
               </li>
             </div>
             <div className="flex-col  justify-between">
               <li className="mb-4 mx-4">
-                <div className="font-semibold text-white hover:text-yellow-500">
+                <div className="text-[10px] md:text-[15px] font-light text-white hover:text-yellow-500">
                   Tender Registration
                 </div>
               </li>
               <li className="mb-4 mx-4">
-                <div className="font-semibold text-white hover:text-yellow-500">
+                <div className="text-[10px] md:text-[15px] font-light text-white hover:text-yellow-500">
                   "CIDA" Registration
                 </div>
               </li>
               <li className="mb-4 mx-4">
-                <div className="font-semibold text-white hover:text-yellow-500">
+                <div className="text-[10px] md:text-[15px] font-light text-white hover:text-yellow-500">
                   Providing Accounting Software
                 </div>
               </li>
@@ -74,15 +74,15 @@ export default function Services() {
         <ul className="list-disc text-white  pl-5 md:flex sm:flex-row">
           <div className="flex-col justify-between">
             <li className="mb-4 mx-4">
-              <div className="font-semibold text-white  hover:text-yellow-500">
+              <div className="text-[10px] md:text-[15px] font-light text-white  hover:text-yellow-500">
                 King Coconut
               </div>
             </li>
             <li className="mb-4 mx-4">
-              <div className="font-semibold text-white  hover:text-yellow-500">Tapioca</div>
+              <div className="text-[10px] md:text-[15px] font-light text-white  hover:text-yellow-500">Tapioca</div>
             </li>
             <li className="mb-4 mx-4">
-              <div className="font-semibold text-white  hover:text-yellow-500">Papaya</div>
+              <div className="text-[10px] md:text-[15px] font-light text-white  hover:text-yellow-500">Papaya</div>
             </li>
           </div>
         </ul>
@@ -96,17 +96,17 @@ export default function Services() {
         <ul className="list-disc text-white pl-5 md:flex sm:flex-row">
           <div className="flex-col justify-between">
             <li className="mb-4 mx-4">
-              <div className="font-semibold text-white hover:text-yellow-500">
+              <div className="text-[10px] md:text-[15px] font-light text-white hover:text-yellow-500">
                 Preparation of agriculture project proposal
               </div>
             </li>
             <li className="mb-4 mx-4">
-              <div className="font-semibold text-white hover:text-yellow-500">
+              <div className="text-[10px] md:text-[15px] font-light text-white hover:text-yellow-500">
                 "TIN" / Temporary "VAT" Registration
               </div>
             </li>
             <li className="mb-4 mx-4">
-              <div className="font-semibold text-white hover:text-yellow-500">
+              <div className="text-[10px] md:text-[15px] font-light text-white hover:text-yellow-500">
                 Coconut board Registration
               </div>
             </li>
@@ -114,17 +114,17 @@ export default function Services() {
 
           <div className="flex-col justify-between">
             <li className="mb-4 mx-4">
-              <div className="font-semibold text-white hover:text-yellow-500">
+              <div className="text-[10px] md:text-[15px] font-light text-white hover:text-yellow-500">
                 Customs registration work
               </div>
             </li>
             <li className="mb-4 mx-4">
-              <div className="font-semibold text-white hover:text-yellow-500">
+              <div className="text-[10px] md:text-[15px] font-light text-white hover:text-yellow-500">
                 Custom clearance and forwarding services
               </div>
             </li>
             <li className="mb-4 mx-4">
-              <div className="font-semibold text-white hover:text-yellow-500">Papaya</div>
+              <div className="text-[10px] md:text-[15px] font-light text-white hover:text-yellow-500">Papaya</div>
             </li>
           </div>
           </ul>
