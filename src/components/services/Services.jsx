b1ab77e84bb3e4743f95f3cf1c6cec12368000c5
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2024-11-12 23:49:22 +0530

    bug fixed

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -7,58 +7,58 @@ export default function Services() {
         What we can offer for you !
       </h1>
       <div className="mx-10">
-        <div className="bg-gray-100 pt-1 px-5 rounded-2xl mb-2 w-full">
-          <h1 className="text-[15px] md:text-[20px] font-bold">
-            Finance Services
+        <div className="bg-blue-950 pt-1 px-5 rounded-2xl mb-2 w-full">
+          <h1 className="text-[15px] text-white md:text-[20px] font-bold">
+            1. Finance Services
           </h1>
-          <ul className="list-disc pl-5 md:flex sm:flex-row">
+          <ul className="list-disc text-white pl-5 md:flex sm:flex-row">
             <div className="flex-col justify-between">
               <li className="mb-4 mx-4">
-                <div className="font-semibold hover:text-yellow-500">
+                <div className="font-semibold text-white hover:text-yellow-500">
                   New Company Registrations
                 </div>
               </li>
               <li className="mb-4 mx-4">
-                <div className="font-semibold hover:text-yellow-500">
+                <div className="font-semibold text-white hover:text-yellow-500">
                   Secretarial Works
                 </div>
               </li>
               <li className="mb-4 mx-4">
-                <div className="font-semibold hover:text-yellow-500">
+                <div className=" text-white font-semibold hover:text-yellow-500">
                   Income Tax Consultation
                 </div>
               </li>
             </div>
             <div className="flex-col  justify-between">
               <li className="mb-4 mx-4">
-                <div className="font-semibold hover:text-yellow-500">
+                <div className="font-semibold text-white hover:text-yellow-500">
                   Company Audit{" "}
                 </div>
               </li>
               <li className="mb-4 mx-4">
-                <div className="font-semibold hover:text-yellow-500">
+                <div className="font-semibold text-white hover:text-yellow-500">
                   Book Keeping
                 </div>
               </li>
               <li className="mb-4 mx-4">
-                <div className="font-semibold hover:text-yellow-500">
+                <div className="font-semibold text-white hover:text-yellow-500">
                   Preparation of Management Accounts & Final Accounts
                 </div>
               </li>
             </div>
             <div className="flex-col  justify-between">
               <li className="mb-4 mx-4">
-                <div className="font-semibold hover:text-yellow-500">
+                <div className="font-semibold text-white hover:text-yellow-500">
                   Tender Registration
                 </div>
               </li>
               <li className="mb-4 mx-4">
-                <div className="font-semibold hover:text-yellow-500">
+                <div className="font-semibold text-white hover:text-yellow-500">
                   "CIDA" Registration
                 </div>
               </li>
               <li className="mb-4 mx-4">
-                <div className="font-semibold hover:text-yellow-500">
+                <div className="font-semibold text-white hover:text-yellow-500">
                   Providing Accounting Software
                 </div>
               </li>
@@ -67,46 +67,46 @@ export default function Services() {
         </div>
       </div>
 
-      <div className="bg-gray-100 pt-1 px-5 mx-10 rounded-2xl mb-2">
-        <h1 className="text-[15px] md:text-[20px] font-bold">
-          Direct & Indirect agricultural exports
+      <div className="bg-blue-950 pt-1 px-5 mx-10 rounded-2xl mb-2">
+        <h1 className="text-[15px] text-white  md:text-[20px] font-bold">
+          2. Direct & Indirect agricultural exports
         </h1>
-        <ul className="list-disc pl-5 md:flex sm:flex-row">
+        <ul className="list-disc text-white  pl-5 md:flex sm:flex-row">
           <div className="flex-col justify-between">
             <li className="mb-4 mx-4">
-              <div className="font-semibold hover:text-yellow-500">
+              <div className="font-semibold text-white  hover:text-yellow-500">
                 King Coconut
               </div>
             </li>
             <li className="mb-4 mx-4">
-              <div className="font-semibold hover:text-yellow-500">Tapioca</div>
+              <div className="font-semibold text-white  hover:text-yellow-500">Tapioca</div>
             </li>
             <li className="mb-4 mx-4">
-              <div className="font-semibold hover:text-yellow-500">Papaya</div>
+              <div className="font-semibold text-white  hover:text-yellow-500">Papaya</div>
             </li>
           </div>
         </ul>
       </div>
 
-      <div className="bg-gray-100 pt-1 px-5 mx-10 rounded-2xl mb-2">
-        <h1 className="text-[15px] md:text-[20px] font-bold">
-          Agricultural export consultancy
+      <div className="bg-blue-950 pt-1 px-5 mx-10 rounded-2xl mb-2">
+        <h1 className="text-[15px] text-white md:text-[20px] font-bold">
+          3. Agricultural export consultancy
         </h1>
         <div className="flex">
-        <ul className="list-disc pl-5 md:flex sm:flex-row">
+        <ul className="list-disc text-white pl-5 md:flex sm:flex-row">
           <div className="flex-col justify-between">
             <li className="mb-4 mx-4">
-              <div className="font-semibold hover:text-yellow-500">
+              <div className="font-semibold text-white hover:text-yellow-500">
                 Preparation of agriculture project proposal
               </div>
             </li>
             <li className="mb-4 mx-4">
-              <div className="font-semibold hover:text-yellow-500">
+              <div className="font-semibold text-white hover:text-yellow-500">
                 "TIN" / Temporary "VAT" Registration
               </div>
             </li>
             <li className="mb-4 mx-4">
-              <div className="font-semibold hover:text-yellow-500">
+              <div className="font-semibold text-white hover:text-yellow-500">
                 Coconut board Registration
               </div>
             </li>
@@ -114,17 +114,17 @@ export default function Services() {
 
           <div className="flex-col justify-between">
             <li className="mb-4 mx-4">
-              <div className="font-semibold hover:text-yellow-500">
+              <div className="font-semibold text-white hover:text-yellow-500">
                 Customs registration work
               </div>
             </li>
             <li className="mb-4 mx-4">
-              <div className="font-semibold hover:text-yellow-500">
+              <div className="font-semibold text-white hover:text-yellow-500">
                 Custom clearance and forwarding services
               </div>
             </li>
             <li className="mb-4 mx-4">
-              <div className="font-semibold hover:text-yellow-500">Papaya</div>
+              <div className="font-semibold text-white hover:text-yellow-500">Papaya</div>
             </li>
           </div>
           </ul>
