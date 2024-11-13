c74a61e9c86eaa4dba0dc3e8eeed5de06c6a9c6b
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2024-11-13 14:38:58 +0530

    bug fixed

--- src/page/about-us/Mission.jsx
+++ src/page/about-us/Mission.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function Mission() {
   return (
     <div className="bg-white mx-10 rounded-2xl mt-3 flex flex-col md:flex-row">
-      <div className="font-poppins text-[40px] px-10 py-5 font-bold  text-center">
+      <div className="font-poppins text-[30px] md:text-[40px] px-10 py-5 font-bold w-[100%] md:w-[40%]  text-center ">
         <h1>Our Mission :</h1>
       </div>
-      <div className="font-poppins text-[40px]  font-thin m-5 rounded-lg bg-gray-100 text-center">
+      <div className="font-poppins text-[20px] md:text-[40px]  font-thin m-5 rounded-lg bg-gray-100 text-center">
         <h1 className="p-5">Help businesses succeed by building great finance solutions!</h1>
       </div>
     </div>
