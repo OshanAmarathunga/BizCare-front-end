b0ab06198d2f8848d4775cbb4c5ef68857218d96
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2024-11-13 14:34:04 +0530

    responsive

--- src/page/about-us/Mission.jsx
+++ src/page/about-us/Mission.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Mission() {
   return (
-    <div className="bg-white mx-10 rounded-2xl mt-3 flex">
-      <div className="font-poppins text-[40px] px-10 py-5 font-bold w-[500px] text-center">
+    <div className="bg-white mx-10 rounded-2xl mt-3 flex flex-col md:flex-row">
+      <div className="font-poppins text-[40px] px-10 py-5 font-bold  text-center">
         <h1>Our Mission :</h1>
       </div>
       <div className="font-poppins text-[40px]  font-thin m-5 rounded-lg bg-gray-100 text-center">
